950cd81b6faaa8d637e5cb0fb188d121ee7e7ff0
samckp <sam.mca.liet@gmail.com>
2020-01-02 09:17:21 +0530

    initial code setup

---
+++ src/main/java/com/kafka/poc/KafkaCamelDemoApplication.java
@@ -0,0 +1,14 @@
+package com.kafka.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCamelDemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KafkaCamelDemoApplication.class, args);
+	}
+
+}
