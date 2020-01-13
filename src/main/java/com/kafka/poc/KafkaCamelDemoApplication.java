0e875a2f0d36d706fbc9863e67c867c9af5eec91
samckp <sam.mca.liet@gmail.com>
2020-01-13 19:35:05 +0530

    updated

--- src/main/java/com/kafka/poc/KafkaCamelDemoApplication.java
+++ src/main/java/com/kafka/poc/KafkaCamelDemoApplication.java
@@ -1,14 +1,15 @@
 package com.kafka.poc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class KafkaCamelDemoApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(KafkaCamelDemoApplication.class, args);
 	}
-
 }
